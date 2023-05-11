a24a11951a7699b06234f5a650f5f6203ebddb35
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-11 22:00:14 +0300

    Initial Commit

---
+++ Thunderstorm/ThunderstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderstormApp.swift
+//  Thunderstorm
+//
+//  Created by Amby on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThunderstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
