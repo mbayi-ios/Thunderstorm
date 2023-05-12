1f277a84859f1d9b9f6bd9b8960740efeed340e3
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-12 19:14:14 +0300

    presenting the add location view

---
+++ Thunderstorm/Views/Add Location/Views/AddLocationView.swift
@@ -0,0 +1,21 @@
+//
+//  AddLocationView.swift
+//  Thunderstorm
+//
+//  Created by Amby on 12/05/2023.
+//
+
+import SwiftUI
+
+struct AddLocationView: View {
+    let viewModel: AddLocationViewModel
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddLocationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddLocationView(viewModel: .init())
+    }
+}
