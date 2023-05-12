d78ca87a3942e75f3d8fcce287c159556fa2c353
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2023-05-12 21:20:23 +0300

    Forward Geocoding with CLGeocoder

--- Thunderstorm/Geocoding/GeocodingClient.swift
+++ Thunderstorm/Geocoding/GeocodingClient.swift
@@ -1,8 +1,35 @@
 import Foundation
+import CoreLocation
 
 final class GeocodingClient: GeocodingService {
+    enum GeocodingError: Error {
+        case invalidAddressString
+        case requestFailed
+    }
+
+    private let geocoder = CLGeocoder()
+
     func geocodeAddressString(_ addressString: String) async throws -> [Location] {
-        Location.previews
+        guard !addressString.isEmpty else {
+            throw GeocodingError.invalidAddressString
+        }
+        do {
+            let placemarks = try await geocoder.geocodeAddressString(addressString)
+            return placemarks.compactMap { placemark -> Location? in
+                guard
+                    let name = placemark.name,
+                    let country = placemark.country,
+                    let coordinate = placemark.location?.coordinate
+
+                else {
+                    return nil
+                }
+                return Location(id: UUID().uuidString, name: name, country: country, latitude: coordinate.latitude, longitude: coordinate.longitude)
+            }
+        } catch {
+            print("unable to geocode address string \(error)")
+            throw GeocodingError.requestFailed
+        }
     }
 
 
